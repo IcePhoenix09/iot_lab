1a156a06568e54c5af6f00651ac171ec16d88f27
IcePhoenix09 <vlasuk.roma04@gmail.com>
2026-03-21 22:57:24 +0200

    Add a simple process_agent_data implementation

--- edge/app/usecases/data_processing.py
+++ edge/app/usecases/data_processing.py
@@ -12,4 +12,20 @@ def process_agent_data(
     Returns:
         processed_data_batch (ProcessedAgentData): Processed data containing the classified state of the road surface and agent data.
     """
-    # Implement it
+    # I will use the z coordinate of the accelerometer to classify the state of the road surface.
+    z_value = agent_data.accelerometer.z
+
+    # This is a very simple classification based on the z value of the accelerometer. 
+    # In futere we can change it to more sophisticated method, such as machine learning, to classify the state of the road surface.
+    if z_value < -5.0:
+        road_state = "hole"
+    elif z_value > 5.0:
+        road_state = "bump"
+    else:
+        road_state = "normal"
+
+    return ProcessedAgentData(
+        road_state=road_state,
+        agent_data=agent_data
+    )
+
