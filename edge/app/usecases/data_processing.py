7b43d3c413160b155f64961ed1faeffa358814cf
IcePhoenix09 <vlasuk.roma04@gmail.com>
2026-03-22 16:55:35 +0200

    feat: Show road state in mqtt explorer
    add state_code along with road_state to display on mqtt explrorer graph

--- edge/app/usecases/data_processing.py
+++ edge/app/usecases/data_processing.py
@@ -17,15 +17,19 @@ def process_agent_data(
 
     # This is a very simple classification based on the z value of the accelerometer. 
     # In futere we can change it to more sophisticated method, such as machine learning, to classify the state of the road surface.
-    if z_value < -5.0:
+    if z_value < 10000:
         road_state = "hole"
-    elif z_value > 5.0:
+        state_code = -1
+    elif z_value > 17000:
         road_state = "bump"
+        state_code = 1
     else:
         road_state = "normal"
+        state_code = 0
 
     return ProcessedAgentData(
         road_state=road_state,
+        state_code=state_code,
         agent_data=agent_data
     )
 
